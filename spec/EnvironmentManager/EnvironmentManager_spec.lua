f258dfcfad017532d703940ca2cda43c9d2746b6
Sam <samdup123@gmail.com>
2018-02-11 10:09:32 -0500

    some tests passing

--- spec/EnvironmentManager/EnvironmentManager_spec.lua
+++ spec/EnvironmentManager/EnvironmentManager_spec.lua
@@ -7,18 +7,38 @@ describe('EnvironmentManager', function()
 
   local entity1 = {image = imageMock1, location = {x = 100, y = 100}}
 
-  it('should be able to initialize with some ennvironment entities and display them', function()
+  function table.clone(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+      copy = {}
+      for orig_key, orig_value in next, orig, nil do
+        copy[table.clone(orig_key)] = table.clone(orig_value)
+      end
+      setmetatable(copy, table.clone(getmetatable(orig)))
+    else -- number, string, boolean, etc
+      copy = orig
+    end
+    return copy
+  end
+
+
+  it('should be able to initialize with some environment entities and display them', function()
     local em
+
     imageMock1.getWidth:should_be_called_with(imageMock1):and_also(imageMock1.getHeight:should_be_called_with(imageMock1))
-    :when(function() em = EnvironmentManager({['entity1'] = entity1}) end)
+    :when(function() em = EnvironmentManager({['entity1'] = {image = entity1.image, location = table.clone(entity1.location)}}) end)
+
     assert.are.same(em.allEntities(), {['entity1'] = entity1})
   end)
 
-  -- it('should be able to request a movement', function()
-  --   local em = EnvironmentManager({['entity1'] = entity1})
-  --
-  --   em.requestMovement('entity1', {deltaX = 100, deltaY = 100})
-  --
-  --   assert.are.same(em.allEntities(), {['entity1'] = {image = entity1.image, location = {x = entity1.location.x + 100, y = entity1.location.y + 100}}})
-  -- end)
+  it('should be able to request a movement', function()
+    local em
+    imageMock1.getWidth:should_be_called_with(imageMock1):and_also(imageMock1.getHeight:should_be_called_with(imageMock1))
+    :when(function() em = EnvironmentManager({['entity1'] = {image = entity1.image, location = table.clone(entity1.location)}}) end)
+
+    assert.is_true(em.requestMovement('entity1', {deltaX = 100, deltaY = 100}))
+
+    assert.are.same(em.allEntities(), {['entity1'] = {image = entity1.image, location = {x = entity1.location.x + 100, y = entity1.location.y + 100}}})
+  end)
 end)
--- src/EnvironmentManager/EnvironmentManager.lua
+++ src/EnvironmentManager/EnvironmentManager.lua
@@ -2,14 +2,23 @@ return function(uneditedEnvironmentEntities)
   local environmentEntities = {}
   for tag, entity in pairs(uneditedEnvironmentEntities) do
     local newEntity = entity
-    newEntity.width = entity.image.getWidth()
-    newEntity.height = entity.image.getHeight()
+    newEntity.width = entity.image:getWidth()
+    newEntity.height = entity.image:getHeight()
     environmentEntities[tag] = newEntity
   end
   return {
-    allEntities = function() return environmentEntities end
-    -- requestMovement = function(entityToMove)
-    --   for _, entity in pairs(environmentEntities) do
-    --   end
+    allEntities = function()
+      return environmentEntities
+    end,
+    requestMovement = function(tag, movement)
+      local entityToMove = environmentEntities[tag]
+      local newLocation = {}
+      newLocation.x = entityToMove.location.x + movement.deltaX
+      newLocation.y = entityToMove.location.y + movement.deltaY
+
+      entityToMove.location.x = newLocation.x
+      entityToMove.location.y = newLocation.y
+      return true
+    end
   }
 end
