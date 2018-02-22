1be5bec22d358ebc9e3748e110b6ebb70c26d816
Sam <samdup123@gmail.com>
2018-02-21 19:30:19 -0500

    bout to run first level

--- main.lua
+++ main.lua
@@ -1,7 +1,16 @@
-local
+local resources = require('src/databaseResources')
 
 function love.load()
+  myWorld = love.physics.newWorld(0, 500, false)
+  local sprites = {}
+  sprites.player = love.graphics.newImage('sprites/player.png')
 
+  tiler = require('lib/tiledWorker/sti')
+  gameMap = tiler('maps/mainMap.lua')
+
+  for _, obj in gameMap.layers['objects'].objects do
+    print(obj.width)
+  end
 end
 
 function love.update(deltaTime)
@@ -10,4 +19,15 @@ end
 
 function love.draw()
 
+end
+
+function createPlatform(x, y, width, height)
+  local platform = {}
+  platform.body = love.physics.newBody(myWorld, x, y, "static")
+  platform.shape = love.physics.newRectangleShape(width / 2, height / 2, width, height)
+  platform.fixture = love.physics.newFixture(platform.body, platform.shape)
+  platform.width = width
+  platform.height = height
+
+  return platform
 end
